40781de54a08933e5dd5f066d78bd367c4707861
Snekussaurier <valentin.schiffl@gmail.com>
2024-11-20 21:35:36 +0100

    🗃️ Update database go file

--- src/database/database.go
+++ src/database/database.go
@@ -8,10 +8,12 @@ import (
 )
 
 type User struct {
-	ID       string `gorm:"type:char(36);primaryKey"`
-	Name     string `gorm:"type:varchar(40);not null" json:"name" binding:"required"`
-	Password string `gorm:"type:char(64);not null" json:"password" binding:"required"`
-	Cards    []Card `gorm:"foreignKey:UserID"` // One-to-Many relationship with Card
+	ID       string  `gorm:"type:char(36);primaryKey"`
+	Name     string  `gorm:"type:varchar(40);not null" json:"name" binding:"required"`
+	Password string  `gorm:"type:char(64);not null" json:"password" binding:"required"`
+	Cards    []Card  `gorm:"foreignKey:UserID"` // One-to-Many relationship with Card
+	Tags     []Tag   `gorm:"foreignKey:UserID"` // One-to-Many relationship with Tag
+	States   []State `gorm:"foreignKey:UserID"` // One-to-Many relationship with State
 }
 
 type Card struct {
@@ -21,35 +23,28 @@ type Card struct {
 	Position    int    `gorm:"not null" json:"position" binding:"required"`
 	StateID     int    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"state_id" binding:"required"`
 	UserID      string `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
-	Tags        []Tag  `gorm:"many2many:card_tags;joinForeignKey:CardID;joinReferences:TagName" json:"tags"`
+	User        User   `gorm:"foreignKey:UserID" binding:"-" json:"-"`
+	State       State  `gorm:"foreignKey:StateID" binding:"-" json:"-"`
+	Tags        []Tag  `gorm:"many2many:card_tags;foreignKey:ID;references:ID" json:"tags"`
 }
 
 type Tag struct {
-	Name  string `gorm:"type:varchar(20);primaryKey" json:"name" binding:"required"`
-	Color string `gorm:"type:varchar(6);not null" json:"color" binding:"required"`
-	Cards []Card `gorm:"many2many:card_tags;joinForeignKey:TagName;joinReferences:CardID" json:"-"`
+	ID     int    `gorm:"primaryKey" json:"id"`
+	Name   string `gorm:"type:varchar(20);unique" json:"name" binding:"required"`
+	Color  string `gorm:"type:varchar(6);not null" json:"color" binding:"required"`
+	UserID string `gorm:"type:char(36);not null" json:"-"`
+	User   User   `gorm:"foreignKey:UserID" binding:"-" json:"-"`
+	Cards  []Card `gorm:"many2many:card_tags;foreignKey:ID;references:ID" json:"-"`
 }
 
 type State struct {
 	ID       int    `gorm:"primaryKey" json:"id"`
 	Name     string `gorm:"type:varchar(20);not null" json:"name" binding:"required"`
-	Position int    `gorm:"not null;unique" json:"position" binding:"required"`
+	Position int    `gorm:"not null" json:"position" binding:"required"`
 	Color    string `gorm:"type:char(6);not null" json:"color" binding:"required"`
+	UserID   string `gorm:"type:char(36);not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
+	User     User   `gorm:"foreignKey:UserID" binding:"-" json:"-"`
 	Cards    []Card `gorm:"foreignKey:StateID" json:"-"` // One-to-Many relationship with Card
-}
-
-type ActivityLog struct {
-	ID        string `gorm:"type:char(36);primaryKey"`
-	CardID    string `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	UserID    string `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Timestamp string `gorm:"type:datetime;not null"`
-	ActionID  int    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-}
-
-type ActivityAction struct {
-	ID         int           `gorm:"primaryKey"`
-	ActionName string        `gorm:"type:varchar(20);not null"`
-	Logs       []ActivityLog `gorm:"foreignKey:ActionID"` // One-to-Many relationship with ActivityLog
 }
 
 var DB *gorm.DB
@@ -61,37 +56,7 @@ func InitializeDB() {
 		log.Fatalf("Failed to open the database: %v", err)
 	}
 
-	if err := DB.AutoMigrate(&User{}, &Card{}, &Tag{}, &State{}, &ActivityLog{}, &ActivityAction{}); err != nil {
+	if err := DB.AutoMigrate(&User{}, &Card{}, &Tag{}, &State{}); err != nil {
 		log.Fatalf("Failed to create tables: %v", err)
-	}
-
-	InitializeStates()
-	InitializeTags()
-}
-
-func InitializeStates() {
-	states := []State{
-		{ID: 1, Name: "Todo", Position: 1, Color: "FF0000"},
-		{ID: 2, Name: "In Progress", Position: 2, Color: "00FF00"},
-		{ID: 3, Name: "Done", Position: 3, Color: "0000FF"},
-	}
-
-	for _, state := range states {
-		if err := DB.Where("id = ?", state.ID).FirstOrCreate(&state).Error; err != nil {
-			log.Fatalf("Failed to create state: %v", err)
-		}
-	}
-}
-
-func InitializeTags() {
-	tags := []Tag{
-		{Name: "Feature", Color: "FF0000"},
-		{Name: "Bug", Color: "00FF00"},
-	}
-
-	for _, tag := range tags {
-		if err := DB.Where("name = ?", tag.Name).FirstOrCreate(&tag).Error; err != nil {
-			log.Fatalf("Failed to create tag: %v", err)
-		}
 	}
 }
